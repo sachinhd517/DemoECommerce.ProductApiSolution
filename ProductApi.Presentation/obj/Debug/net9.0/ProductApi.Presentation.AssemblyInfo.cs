78609ec6ecf5f3a478127682385f8b953e7ab2e4
sachinhd517 <sachinhd517@gmail.com>
2026-04-03 16:59:50 +0530

    Product API

--- ProductApi.Presentation/obj/Debug/net9.0/ProductApi.Presentation.AssemblyInfo.cs
+++ ProductApi.Presentation/obj/Debug/net9.0/ProductApi.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+422b5e7e2791c03556622409a19eaab9c05c0201")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c27761181e1cb9af34a17924951bfae2aa7fac2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
